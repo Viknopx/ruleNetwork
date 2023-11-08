c93ac305f9b939381474a0582610141bd3aa6285
Viknopx <chreadppp@gmail.com>
2023-11-08 15:27:10 +0800

    444

--- clash/script.sh
+++ clash/script.sh
@@ -1,13 +1,9 @@
 #!/bin/bash
 
-sed -i "/proxies/a\${HK_PROXY}" proxy_temp.yaml
+sed -i '/proxies:/a\'"${HK_PROXY}"'' proxy_temp.yaml
 
 #sed -i "s#SED_HK_PROXY_NAME#${HK_PROXY_NAME}#g" proxy_temp.yaml
 #sed -i "s#SED_HK_PROXY_SERVER#${HK_PROXY_SERVER}#g" proxy_temp.yaml
 #sed -i "s#SED_HK_PROXY_SECRETS#${HK_PROXY_SECRETS}#g" proxy_temp.yaml
 cat proxy_temp.yaml config_rule.yaml >clash.yaml
 cat clash.yaml
-
-
-
-#sed  -i  "/proxies/a\  - 添加的内容"  1.yaml
