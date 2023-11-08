9a5441828a8f4ec995b5a6532c8a2cf720baee3d
Viknopx <chreadppp@gmail.com>
2023-11-08 11:30:08 +0800

    2

---
+++ clash/script.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+sed -i "s#SED_HK_PROXY_NAME#${HK_PROXY_NAME}#g" proxy_temp.yaml
+sed -i "s#SED_HK_PROXY_SERVER#${HK_PROXY_SERVER}#g" proxy_temp.yaml
+sed -i "s#SED_HK_PROXY_SECRETS#${HK_PROXY_SECRETS}#g" proxy_temp.yaml
+cat proxy_temp.yaml config_rule.yaml >clash.yaml
+cat clash.yaml
