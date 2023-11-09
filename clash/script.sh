8f507e9cb5e5e7f380eb0f919818f6c8e20aa92b
Viknopx <chreadppp@gmail.com>
2023-11-09 10:45:00 +0800

    add rclone

--- clash/script.sh
+++ clash/script.sh
@@ -1,9 +1,22 @@
 #!/bin/bash
 
-sed -i '/proxies:/a\'"${HK_PROXY}"'' proxy_temp.yaml
 
-#sed -i "s#SED_HK_PROXY_NAME#${HK_PROXY_NAME}#g" proxy_temp.yaml
-#sed -i "s#SED_HK_PROXY_SERVER#${HK_PROXY_SERVER}#g" proxy_temp.yaml
-#sed -i "s#SED_HK_PROXY_SECRETS#${HK_PROXY_SECRETS}#g" proxy_temp.yaml
+
+SEND_TITLE="Clash配置更新"
+SEND_MESSAGE="$HK_PROXY \n更新完成。"
+
+
+sed -i '/proxies:/a\'"${HK_PROXY}"'' proxy_temp.yaml
 cat proxy_temp.yaml config_rule.yaml >clash.yaml
-cat clash.yaml
+
+
+
+echo "发送通知！"
+curl "$SEND_SERVER/message?token=$SEND_TOKEN" -F "title=$SEND_TITLE" -F "message=$SEND_MESSAGE" -F "priority=5"
+
+
+
+echo "同步配置"
+apt install rclone -y
+envsubst < ../rclone.conf > ~/.config/rclone/rclone.conf 
+rclone copy clash.yaml rclone:ops-software:/temp
